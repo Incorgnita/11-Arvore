44ff55768e27ec683f8c02dde0c49782cf1d63e8
Incorgnita <89551415+Incorgnita@users.noreply.github.com>
2022-11-01 10:53:54 -0300

    Update ArvoreBinaria.cpp

--- ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
@@ -79,7 +79,7 @@ void menu()
 void inicializar()
 {
 
-	// provisÛrio porque n„o libera a memoria usada pela arvore
+	// provis√≥rio porque n√£o libera a memoria usada pela arvore
 	NO* raiz = NULL;
 
 	cout << "Arvore inicializada \n";
@@ -177,8 +177,24 @@ void exibirElementosArvore(NO* no)
 
 void buscarElementoArvore(NO* no, int valor)
 {
-	
+	if (no == NULL)
+	{
+		return;
+	}
+	else if (valor == no->valor)
+	{
+		cout << "Achou: " << no->valor << endl;
+	}
+	else if (valor < no->valor)
+	{
+		return buscarElementoArvore(no->esq, valor);
 
+	}
+	else
+	{
+		return buscarElementoArvore(no->dir, valor);
+	}
+	
 }
 
 
